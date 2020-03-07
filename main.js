441ed38091728cd2a152bb3a805c9a3d30caf1bb
har4enko <pan4enko48@gmail.com>
2020-03-07 09:20:43 +0200

    HW#10 - done

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const letters = document.getElementsByClassName('box');
 [...letters].forEach(letter => {
   letter.addEventListener('mousedown', function() {
     this.style.border = '2px solid orange';
+    const audio = new Audio();
+    audio.src = `sounds/${letter.id}.mp3`;
+    audio.play();
   });
 });
 [...letters].forEach(letter => {
@@ -9,23 +12,20 @@ const letters = document.getElementsByClassName('box');
     this.style.border = '2px solid black';
   });
 });
-[...letters].forEach(letter => {
-  letter.addEventListener('mousedown', function() {
-    const audio = new Audio();
-    audio.src = `sounds/${letter.id}.mp3`;
-    audio.play();
-  });
-});
 
 document.addEventListener('keydown', event => {
-  const a = document.querySelector(`.${event.key.toUpperCase()}`);
-  a.style.border = '2px solid orange';
-  const audio = new Audio();
-  audio.src = `sounds/${a.id}.mp3`;
-  audio.play();
+  const a = document.getElementById(`${event.keyCode}`);
+  if (a) {
+    a.style.border = '2px solid orange';
+    const audio = new Audio();
+    audio.src = `sounds/${a.id}.mp3`;
+    audio.play();
+  }
 });
 
 document.addEventListener('keyup', event => {
-  const a = document.querySelector(`.${event.key.toUpperCase()}`);
-  a.style.border = '2px solid black';
+  const a = document.getElementById(`${event.keyCode}`);
+  if (a) {
+    a.style.border = '2px solid black';
+  }
 });
